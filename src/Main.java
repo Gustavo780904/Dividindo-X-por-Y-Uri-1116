eadd4dbf442f6eca87cf7532b779241407759f81
Gustavo780904 <gustavodutrasantos@gmail.com>
2020-06-13 10:02:43 -0300

    Projeto finalizado

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,24 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		int X, Y, N;
+
+		int X = 0, Y = 0, N = 0;
 		double divisao;
 		
+		N = sc.nextInt();
 		
 		
-		
-		
-		
-		
-		
+		for (int i = 0; i < N; i++) {
+			X = sc.nextInt();
+			Y = sc.nextInt();
+			if (Y != 0) {
+				divisao = (double) X / Y;
+				System.out.printf("%.1f%n", divisao);
+			} else {
+				System.out.println("divisao impossivel");
+			}
+		}
+
 		sc.close();
 
 	}
